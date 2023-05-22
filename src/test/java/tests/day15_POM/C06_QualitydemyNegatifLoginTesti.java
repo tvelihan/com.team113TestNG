04af461bf61d10b4882fa40746fdf522f5098d1d
tvelihan <velihanturk69@gmail.com>
2023-05-22 21:06:43 +0300

    22.05.2023

--- src/test/java/tests/day15_POM/C06_QualitydemyNegatifLoginTesti.java
+++ src/test/java/tests/day15_POM/C06_QualitydemyNegatifLoginTesti.java
@@ -30,4 +30,26 @@ public class C06_QualitydemyNegatifLoginTesti {
         Driver.closeDriver();
     }
 
+    @Test
+    public void gecersizIsimSifreTesti(){
+        // QUALITYDEMY ANA SAYFAYA GIDIN
+        Driver.getDriver().get(ConfigReader.getProperty("qdUrl"));
+        // login linkine tiklayin
+
+        QualitydemyPage qualitydemyPage = new QualitydemyPage();
+        qualitydemyPage.ilkLoginLinki.click();
+        // gecersiz username ve geçerli sifre yi ilgili kutulara yazin
+        qualitydemyPage.emailKutusu.sendKeys(ConfigReader.getProperty("qdGecersizUsername"));
+        qualitydemyPage.passwordKutusu.sendKeys(ConfigReader.getProperty("qdGecerliPassword"));
+
+        // login butonuna basin
+        qualitydemyPage.loginButonu.click();
+
+        // basarili olarak giris yapilamadigini test edin
+        Assert.assertTrue(qualitydemyPage.emailKutusu.isEnabled());
+        // sayfayi kapatin
+
+        Driver.closeDriver();
+    }
+
 }
